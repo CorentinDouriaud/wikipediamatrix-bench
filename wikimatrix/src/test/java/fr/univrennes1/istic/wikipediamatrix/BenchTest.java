75a62ea1510e30084286d56cf25bc8e51927a726
HP-ZBOOK-05 <HP-ZBOOK-05@192.168.150.173>
2018-12-06 16:05:17 +0100

    Sauvegarde 2

--- wikimatrix/src/test/java/fr/univrennes1/istic/wikipediamatrix/BenchTest.java
+++ wikimatrix/src/test/java/fr/univrennes1/istic/wikipediamatrix/BenchTest.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 //Test de mise à jour
 public class BenchTest {
@@ -23,9 +25,6 @@ public class BenchTest {
 		// directory where CSV files are exported (HTML extractor) 
 		String outputDirHtml = "output" + File.separator + "html" + File.separator;
 		assertTrue(new File(outputDirHtml).isDirectory());
-		// directory where CSV files are exported (Wikitext extractor) 
-		String outputDirWikitext = "output" + File.separator + "wikitext" + File.separator;
-		assertTrue(new File(outputDirWikitext).isDirectory());
 		
 		File file = new File("inputdata" + File.separator + "wikiurls.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -34,16 +33,10 @@ public class BenchTest {
 	    while ((url = br.readLine()) != null) {
 	       String wurl = BASE_WIKIPEDIA_URL + url; 
 	       System.out.println("Wikipedia url: " + wurl);
-	       // TODO: do something with the Wikipedia URL 
-	       // (ie extract relevant tables for correct URL, with the two extractors)
-		    
-	       
-	       // for exporting to CSV files, we will use mkCSVFileName 
-	       // example: for https://en.wikipedia.org/wiki/Comparison_of_operating_system_kernels
-	       // the *first* extracted table will be exported to a CSV file called 
-	       // "Comparison_of_operating_system_kernels-1.csv"
+	       JSoupTest jsouptest = new JSoupTest();
 	       String csvFileName = mkCSVFileName(url, 1);
 	       System.out.println("CSV file name: " + csvFileName);
+	       jsouptest.testUrlCanon(wurl, csvFileName);
 	       // the *second* (if any) will be exported to a CSV file called
 	       // "Comparison_of_operating_system_kernels-2.csv"
 
